c2a034919b656565e6b3683b89ae65fa886b0266
Majd <majdbitar2003@gmail.com>
2024-08-18 23:43:41 +0300

    Added delete mapping to deleteProfile method in profile service

--- autosync/src/main/java/com/autosync/autosync/Controllers/ProfileController.java
+++ autosync/src/main/java/com/autosync/autosync/Controllers/ProfileController.java
@@ -3,12 +3,15 @@ package com.autosync.autosync.Controllers;
 
 import com.autosync.autosync.Models.ProfileModel;
 import com.autosync.autosync.Services.ProfileService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -40,6 +43,16 @@ public class ProfileController {
     public ResponseEntity<ProfileModel> updateProfile(@PathVariable UUID profileId, @RequestBody ProfileModel profileDetails) {
         ProfileModel updatedProfile = profileService.updateProfile(profileId, profileDetails);
         return new ResponseEntity<>(updatedProfile, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{profileId}")
+    public ResponseEntity<Map<String, String>> deleteProfile(@PathVariable UUID profileId) {
+        profileService.deleteProfile(profileId);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Profile Deleted");
+
+        return new ResponseEntity<>(response, HttpStatus.GONE);
     }
 
 }
