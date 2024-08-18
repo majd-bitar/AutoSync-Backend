2bfbbed0f00e955a47d2b57ac77d1b4489d72aa3
Majd <majdbitar2003@gmail.com>
2024-08-18 11:45:06 +0300

    Initialized boot-spring project with maven and added application properties to the application.properties file

---
+++ autosync/src/main/java/com/autosync/autosync/AutosyncApplication.java
@@ -0,0 +1,13 @@
+package com.autosync.autosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosyncApplication.class, args);
+	}
+
+}
---
+++ autosync/src/test/java/com/autosync/autosync/AutosyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autosync.autosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
