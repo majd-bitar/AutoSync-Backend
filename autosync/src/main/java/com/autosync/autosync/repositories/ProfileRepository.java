8631d78ffddc4768d54d3edbf835a1903ead3200
Majd <majdbitar2003@gmail.com>
2024-08-18 16:49:12 +0300

    Added profile repository

---
+++ autosync/src/main/java/com/autosync/autosync/repositories/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.autosync.autosync.repositories;
+
+import com.autosync.autosync.models.ProfileModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<ProfileModel, UUID> {
+}
