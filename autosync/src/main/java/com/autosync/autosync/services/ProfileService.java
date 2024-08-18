39d2e09522b1fc72726ef6982856d4b3871f5d5a
Majd <majdbitar2003@gmail.com>
2024-08-18 20:51:29 +0300

    Added getProfileById to profile service

--- autosync/src/main/java/com/autosync/autosync/services/ProfileService.java
+++ autosync/src/main/java/com/autosync/autosync/services/ProfileService.java
@@ -4,6 +4,10 @@ import com.autosync.autosync.models.ProfileModel;
 import com.autosync.autosync.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.autosync.autosync.utils.CustomExceptions;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProfileService {
@@ -14,5 +18,16 @@ public class ProfileService {
     public ProfileModel createProfile(ProfileModel profile){
         return profileRepository.save(profile);
     }
+
+    public ProfileModel getProfileById(UUID profileId) throws CustomExceptions.ProfileNotFoundException{
+        Optional<ProfileModel> profile = profileRepository.findById(profileId);
+        if(profile.isPresent()){
+            return profile.get();
+        }
+        else{
+            throw new CustomExceptions.ProfileNotFoundException("Profile not found");
+        }
+    }
+
 
 }
