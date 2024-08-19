f4759837433170e3a9e0f7450e43b544725e7790
Majd <majdbitar2003@gmail.com>
2024-08-19 17:04:20 +0300

    Added Authorization layer so we have protected routes for each user

--- autosync/src/main/java/com/autosync/autosync/Configurations/SecurityConfig.java
+++ autosync/src/main/java/com/autosync/autosync/Configurations/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig  {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
