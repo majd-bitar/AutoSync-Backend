134f8e2463cf100f5e015680fb813469f4cda2aa
Majd <majdbitar2003@gmail.com>
2024-08-19 13:04:42 +0300

    Added a method to generate a JWT token in login service

--- autosync/src/main/java/com/autosync/autosync/Services/LoginService.java
+++ autosync/src/main/java/com/autosync/autosync/Services/LoginService.java
@@ -1,10 +1,16 @@
 package com.autosync.autosync.Services;
 
 
+import com.autosync.autosync.Models.LoginModel;
 import com.autosync.autosync.Repositories.LoginRepository;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class LoginService {
@@ -13,4 +19,20 @@ public class LoginService {
 
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private final String JWTSecret = dotenv.get("JWT_SECRET_KEY");
+
+    // Method to generate a JWT token
+    private String generateJwtToken(LoginModel login) {
+        return Jwts.builder()
+                .setSubject(login.getUsername())
+                .claim("id",login.getLoginId())
+                .claim("role", login.getRole())
+                .claim("status", login.getStatus())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour expiration
+                .signWith(SignatureAlgorithm.HS256, JWTSecret)
+                .compact();
+    }
 }
