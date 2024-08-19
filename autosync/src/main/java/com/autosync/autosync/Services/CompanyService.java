bb2259c5b92dfb4069e89dc98a348f5274234b5b
Majd <majdbitar2003@gmail.com>
2024-08-19 21:46:15 +0300

    Added getAllCompanies and updateCompany to CompanyService

--- autosync/src/main/java/com/autosync/autosync/Services/CompanyService.java
+++ autosync/src/main/java/com/autosync/autosync/Services/CompanyService.java
@@ -9,6 +9,7 @@ import com.autosync.autosync.Repositories.MechanicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,6 +33,28 @@ public class CompanyService {
         Optional<CompanyModel> companyOptional = companyRepository.findById(companyId);
         if (companyOptional.isPresent()) {
             return companyOptional.get();
+        } else {
+            throw new CustomExceptions.CompanyNotFoundException("Company not found.");
+        }
+    }
+    public List<CompanyModel> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    public CompanyModel updateCompany(UUID companyId, CompanyModel companyDetails) throws CustomExceptions.CompanyNotFoundException {
+        Optional<CompanyModel> companyOptional = companyRepository.findById(companyId);
+        if (companyOptional.isPresent()) {
+            CompanyModel existingCompany = companyOptional.get();
+
+            if(existingCompany.getCompanyAddress()!=null)
+                existingCompany.setCompanyAddress(companyDetails.getCompanyAddress());
+            if(existingCompany.getCompanyLogo()!=null)
+                existingCompany.setCompanyLogo(companyDetails.getCompanyLogo());
+            if(existingCompany.getCompanyName()!=null)
+                existingCompany.setCompanyName(companyDetails.getCompanyName());
+            if(existingCompany.getCompanyPhoneNumber()!=null)
+                existingCompany.setCompanyPhoneNumber(companyDetails.getCompanyPhoneNumber());
+            return companyRepository.save(existingCompany);
         } else {
             throw new CustomExceptions.CompanyNotFoundException("Company not found.");
         }
